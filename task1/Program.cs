5f6f4c6fa7c3db3c0347a1adfd309ad5fb42ce83
Vika <vika.tulnikova@bk.ru>
2023-01-23 16:03:01 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите размер массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+RandonNumbers(numbers);
+Console.WriteLine("В этом массиве: ");
+Console.Write("[");
+Console.Write(string.Join(", ", numbers));
+Console.WriteLine("]");
+
+void RandonNumbers(int[] numbers)
+{
+    for(int i = 0; i < size; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+if (numbers[i] % 2 == 0)
+count++;
+}
+Console.WriteLine($"из {numbers.Length} чисел {count} четных");
