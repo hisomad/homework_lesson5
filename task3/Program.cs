91a0aa883eb46762284069fc074751e7709b1397
Vika <vika.tulnikova@bk.ru>
2023-01-23 16:10:01 +0300

    add task3

---
+++ task3/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Введите размер массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[arraySize];
+FillArrayRandomNumbers(array);
+Console.WriteLine("Массив: ");
+Console.Write("[");
+Console.Write(string.Join(", ", array));
+Console.WriteLine("]");
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+        {
+            max = array[i];
+        }
+    if (array[i] < min)
+        {
+            min = array[i];
+        }
+}
+
+Console.WriteLine($"Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+
+void FillArrayRandomNumbers(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        {
+            array[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+
