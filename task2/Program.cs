07f115bee6382bc1697353c897a35424f36b1904
Vika <vika.tulnikova@bk.ru>
2023-01-23 16:07:19 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите размер массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[arraySize];
+FillArrayRandomNumbers(array);
+Console.WriteLine("Массив: ");
+Console.Write("[");
+Console.Write(string.Join(", ", array));
+Console.WriteLine("]");
+int result = 0;
+
+for (int i = 0; i < array.Length; i+=2)
+    result = result + array[i];
+
+    Console.WriteLine($"Сумма элементов на нечётных позициях = {result}");
+
+void FillArrayRandomNumbers(int[] array)
+{
+    Random rnd = new Random();
+for(int i = 0; i < array.Length; i++)
+    array[i] = rnd.Next(1,10);
+        
+}
+
